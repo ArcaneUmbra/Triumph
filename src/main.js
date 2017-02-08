faad95ee2b9c5273d3d5c0a08ec595acd9a9bbc4
ArcaneUmbra <arcaneumbra@gmail.com>
2017-02-08 21:48:41 +1000

    Set up canvas

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// Get canvas and set context 
+
+var gc = document.getElementById('gc');
+var ctx = gc.getContext('2d');
+
+// Set the canvas to fill window 
+
+gc.width = window.innerWidth;
+gc.height = window.innerHeight;
